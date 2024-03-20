447b4541401721594d1c720a29a83245ec898c0e
dmitriy <dimaibraev3008@gmail.com>
2024-03-20 16:17:24 +0300

    extractor modification

--- src/algo/ComponentsBuilderAroundPivot2.java
+++ src/algo/ComponentsBuilderAroundPivot2.java
@@ -28,11 +28,11 @@ public class ComponentsBuilderAroundPivot2 {
 
     public static List<ConnectedComponentWithPivots> splitStrategy(BigLong2ShortHashMap hm,
                                                                    int k, BigLong2ShortHashMap pivot, BigLong2ShortHashMap pivot2,
-                                                                   String statFP, Logger logger) throws FileNotFoundException {
+                                                                   String statFP, Logger logger, int minPivots, double pivotsQuotient) throws FileNotFoundException {
 
         logger.debug("split-strategy");
 
-        ComponentsBuilderAroundPivot2 builder = new ComponentsBuilderAroundPivot2(k, statFP, logger);
+        ComponentsBuilderAroundPivot2 builder = new ComponentsBuilderAroundPivot2(k, statFP, logger, minPivots, pivotsQuotient);
         builder.run(hm, pivot, pivot2);
         return builder.ans;
     }
@@ -42,12 +42,17 @@ public class ComponentsBuilderAroundPivot2 {
     final String statFP;
     final private Logger logger;
 
+    final private int minPivots;
+    final private double pivotsQuotient;
 
-    public ComponentsBuilderAroundPivot2(int k, String statFP, Logger logger) {
+
+    public ComponentsBuilderAroundPivot2(int k, String statFP, Logger logger, int minPivots, double pivotsQuotient) {
         this.ans = new ArrayList<ConnectedComponentWithPivots>();
         this.k = k;
         this.statFP = statFP;
         this.logger = logger;
+        this.minPivots = minPivots;
+        this.pivotsQuotient = pivotsQuotient;
     }
 
     private void run(BigLong2ShortHashMap hm, BigLong2ShortHashMap pivot, BigLong2ShortHashMap pivot2) throws FileNotFoundException {
@@ -349,12 +354,9 @@ public class ComponentsBuilderAroundPivot2 {
         return comp;
     }
 
-    private static final int MIN_PIVOTS = 100;
-
-
     private boolean goodPath(int pivotCnt, int pivot2Cnt) {
         logger.debug(pivotCnt + " " + pivot2Cnt);
-        return pivotCnt > pivot2Cnt && pivotCnt >= MIN_PIVOTS;
+        return (double) pivotCnt * pivotsQuotient > (double) pivot2Cnt && pivotCnt >= minPivots;
     }
 
     private boolean dfs(long startKmer, long parentKmer, Long2ShortHashMapInterface hm,
